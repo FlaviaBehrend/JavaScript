a9958d15ae9ddb6f70adea291df8c7b96a9ae6a0
FlaviaBehrend <flahbehrend@gmail.com>
2025-11-17 16:25:57 -0300

    atualizado

--- atividade_avaliativa/js/atvAvaliativa.js
+++ atividade_avaliativa/js/atvAvaliativa.js
@@ -17,7 +17,7 @@ function adicionarNota() {
 
     let cor;
     if (contadorNotas === 1) cor = "blue";
-    else if (contadorNotas === 2) cor = "#4a9207";
+    else if (contadorNotas === 2) cor = "green";
     else if (contadorNotas === 3) cor = "purple";
 
     if (contadorNotas > 3) {
@@ -43,23 +43,42 @@ function adicionarNota() {
 }
 
 function removerTodasNotas() {
+    if(urgentes == "" && naoUrgentes == ""){
+        alert("Lista vazia, insira uma nota.")
+        return;
+
+    }
+    else{
     document.getElementById("urgentes").innerHTML = "";
     document.getElementById("naoUrgentes").innerHTML = "";
 
     urgentes = [];
     naoUrgentes = [];
     contadorNotas = 0;
+    }
 }
 
 function removerNotasUrgentes() {
+    if(urgentes == ""){
+        alert("Não há notas para remover.")
+        return;
+
+    }
     const lista = document.getElementById("urgentes");
     if (lista.lastChild) {
         lista.removeChild(lista.lastChild);
         urgentes.pop();
+
     }
+    
 }
 
 function removerNotas() {
+    if(naoUrgentes == ""){
+        alert("Lista vazia, insira uma nota.")
+        return;
+
+    }
     const lista = document.getElementById("naoUrgentes");
     if (lista.lastChild) {
         lista.removeChild(lista.lastChild);
